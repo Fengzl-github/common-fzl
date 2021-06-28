689aefa1fefc79cf33d9a945c09734336199d005
fengzl <18233521351@163.com>
2021-06-28 16:23:22 +0800

    -

---
+++ src/main/java/com/cn/common/utils/RemoteAddrUtils.java
@@ -0,0 +1,42 @@
+package com.cn.common.utils;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ *@Author fengzhilong
+ *@Date 2021/6/28 16:21
+ *@Desc IP地址相关工具类
+ **/
+public class RemoteAddrUtils {
+
+    public static String client(HttpServletRequest request) {
+        String xff = request.getHeader("x-forwarded-for");
+        if (xff == null) {
+            xff = request.getRemoteAddr();
+        }
+        return xff;
+    }
+
+    public static String getIpAddr(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("http_client_ip");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ip != null && ip.indexOf(",") != -1) {
+            ip = ip.substring(ip.lastIndexOf(",") + 1, ip.length()).trim();
+        }
+        return ip;
+    }
+}
